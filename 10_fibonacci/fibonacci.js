a6ae5ecbde2ac1a724173f23698e9d32d6e33782
JakeSturges <jakersturges@gmail.com>
2024-04-27 13:18:04 -0700

    solved problems

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    let firstNum = 0;
+    let secondNum = 1;
+    let nextNum;
 
+    for (let i = 1; i < n; i++) {
+        nextNum = firstNum + secondNum;
+        firstNum = secondNum;
+        secondNum = nextNum;
+    }
+    return nextNum;
 };
 
 // Do not edit below this line
